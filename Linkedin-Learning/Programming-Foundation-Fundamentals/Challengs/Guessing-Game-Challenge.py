dace4a8dbb161a76e44394ea227d3e78cfe28684
MohammedSabry2022 <102177371+MohammedSabry2022@users.noreply.github.com>
2022-03-23 14:21:47 +0200

    Update Guessing-Game-Challenge.py

--- Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Guessing-Game-Challenge.py
+++ Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Guessing-Game-Challenge.py
@@ -1,1 +1,10 @@
+# Guessing game
+guess = input("What's my favorite food? ")
+
+if guess == "cookies":
+    print("Yep! So amazing!")
+else:
+    print("Yuck! That’s not it!")
+
+print("Thanks for playing!")
 
